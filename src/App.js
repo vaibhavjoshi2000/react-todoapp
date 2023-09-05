574883843931a2003fa78a948368373bd7b0cc00
vaibhavjoshi2000 <vaibhavjoshibawa@gmail.com>
2023-09-05 11:01:11 +0530

    Changes in App.js, package-lock.json, package.json

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => {
 
   return (
     <div className="container">
-    <BrowserRouter>
+    <BrowserRouter basename = "/react-todoapp">
       <Routes>
-        <Route exact path="/" element={<Create />}></Route>
-        <Route exact path="/read" element={<Read />}></Route>
-        <Route exact path = "/update" element = {<Update />}></Route>
+        <Route exact path="/react-todoapp" element={<Create />}></Route>
+        <Route path="/read" element={<Read />}></Route>
+        <Route path = "/update" element = {<Update />}></Route>
       </Routes>
     </BrowserRouter>
     </div>
